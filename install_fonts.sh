1d5854cf3a575fa754451f7b25ba0518ed61850c
Jo Carllyle <JamesBrosy@users.noreply.github.com>
2024-11-19 17:17:28 +0800

    Update scripts

--- install_fonts.sh
+++ install_fonts.sh
@@ -10,6 +10,12 @@ fi
 DIR=$(dirname "$(readlink -f "$0")")
 FONTS_DIR="/usr/share/fonts/wps-office"
 
-mv -v "$DIR"/fonts/*.ttf "$FONTS_DIR"
+if [ ! -d "$FONTS_DIR" ]; then
+  echo -e "\e[31m[WARNING] Please install WPS Office First!!!\e[0m\n"
+  exit 1
+fi
+
+echo "Installing fonts..."
+install -Dm644 "$DIR"/fonts/*.ttf -t "$FONTS_DIR"
 
 echo -e "\e[33mInstall Fonts Successfully!!!\e[0m"
--- install_libs.sh
+++ install_libs.sh
@@ -10,9 +10,14 @@ fi
 DIR=$(dirname "$(readlink -f "$0")")
 LIB_DIR="/opt/kingsoft/wps-office/office6"
 
-mv -v "$DIR/libfreetype/$(uname -m)/libfreetype.so.6.*" "$LIB_DIR"
+if [ ! -d "$LIB_DIR" ]; then
+  echo -e "\e[31m[WARNING] Please install WPS Office First!!!\e[0m\n"
+  exit 1
+fi
 
-echo "Making link libfreetype.so.6.18.3 -> libfreetype.so.6"
+install -Dm755 "$DIR"/libfreetype/$(uname -m)/libfreetype.so.6.* -t "$LIB_DIR"
+
+echo "Making link libfreetype.so.6.* -> libfreetype.so.6"
 cd "$LIB_DIR" && ln -s libfreetype.so.6.* libfreetype.so.6
 
 echo -e "\e[33mInstall Libfreetype Successfully!!!\e[0m"
